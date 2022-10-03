c2be5b06b55cee902ffdf19c35ffd487a7dee77d
Ксандр <@LAPTOP-N68EL3GU>
2022-10-03 19:59:42 +0300

    Добавьте файлы проекта.

---
+++ Schizofrenia/Program.cs
@@ -0,0 +1,16 @@
+﻿Random rnd = new Random();
+while (true)
+{
+    int value = rnd.Next(0, 100);
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num == value)
+    {
+        Console.WriteLine("ура, верное число, ты смог");
+        break;
+    }
+    else
+    {
+        Console.WriteLine("введите новое число, это число не совсем то что нам нужно");
+    }
+
+}
